941b6528cfc7ff83cd28d6a4478bbd044410f54b
woong <koohw0109@gmail.com>
2023-05-29 16:43:50 +0900

    modify some

--- lib/features/videos/widgets/video_comments.dart
+++ lib/features/videos/widgets/video_comments.dart
@@ -36,7 +36,7 @@ class _VideoCommentsState extends State<VideoComments> {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Container(
-      height: size.height * 0.75,
+      height: size.height * 0.73,
       clipBehavior: Clip.hardEdge,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(
@@ -49,17 +49,23 @@ class _VideoCommentsState extends State<VideoComments> {
           toolbarHeight: 45,
           backgroundColor: Colors.grey.shade50,
           automaticallyImplyLeading: false,
+          centerTitle: true,
           title: const Text(
             "22796 comments",
             style: TextStyle(
-              fontSize: Sizes.size14 + Sizes.size1,
-              fontWeight: FontWeight.w400,
+              fontSize: Sizes.size14,
+              color: Colors.black,
+              fontWeight: FontWeight.w500,
             ),
           ),
           actions: [
             IconButton(
-                onPressed: _onClosePressed,
-                icon: const Icon(Icons.close_rounded)),
+              onPressed: _onClosePressed,
+              icon: const Icon(Icons.close_rounded),
+            ),
+            const SizedBox(
+              width: Sizes.size4,
+            ),
           ],
         ),
         body: GestureDetector(
@@ -74,8 +80,8 @@ class _VideoCommentsState extends State<VideoComments> {
                   padding: const EdgeInsets.only(
                     top: Sizes.size10,
                     bottom: Sizes.size20 + Sizes.size96,
-                    left: Sizes.size16,
-                    right: Sizes.size16,
+                    left: Sizes.size14,
+                    right: Sizes.size14,
                   ),
                   itemCount: 10,
                   itemBuilder: (context, index) => Row(
@@ -85,7 +91,7 @@ class _VideoCommentsState extends State<VideoComments> {
                         radius: 18,
                         child: Text("AA"),
                       ),
-                      Gaps.h10,
+                      Gaps.h8,
                       Expanded(
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
@@ -93,30 +99,76 @@ class _VideoCommentsState extends State<VideoComments> {
                             Text(
                               "woong",
                               style: TextStyle(
-                                fontWeight: FontWeight.bold,
+                                fontWeight: FontWeight.w500,
                                 fontSize: Sizes.size14,
-                                color: Colors.grey.shade500,
+                                color: Colors.grey.shade600,
                               ),
                             ),
-                            Gaps.v3,
+                            Gaps.v5,
                             const Text(
-                                "That's not it l've seen the same thing but also in a cave"),
+                              "That's not it l've seen the same thing but also in a cave",
+                              style: TextStyle(
+                                fontSize: Sizes.size16,
+                              ),
+                            ),
+                            Gaps.v8,
+                            Row(
+                              children: [
+                                Text(
+                                  "1-18",
+                                  style: TextStyle(
+                                    color: Colors.grey.shade700,
+                                    fontWeight: FontWeight.w300,
+                                    fontSize: Sizes.size12 + Sizes.size1,
+                                  ),
+                                ),
+                                Gaps.h10,
+                                Text(
+                                  "Reply",
+                                  style: TextStyle(
+                                    color: Colors.grey.shade600,
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: Sizes.size14,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Gaps.v10,
+                            Row(
+                              children: const [
+                                Text(
+                                  "View replies (194)",
+                                  style: TextStyle(
+                                    color: Colors.black54,
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: Sizes.size14,
+                                  ),
+                                ),
+                                Gaps.h3,
+                                FaIcon(
+                                  FontAwesomeIcons.chevronDown,
+                                  size: Sizes.size14,
+                                  color: Colors.black45,
+                                ),
+                              ],
+                            ),
                           ],
                         ),
                       ),
                       Gaps.h10,
                       Column(
                         children: [
+                          const SizedBox(height: Sizes.size4),
                           FaIcon(
                             FontAwesomeIcons.heart,
-                            size: Sizes.size20,
-                            color: Colors.grey.shade500,
+                            size: Sizes.size20 + Sizes.size1,
+                            color: Colors.grey.shade600,
                           ),
                           Gaps.v2,
                           Text(
                             "52.2K",
                             style: TextStyle(
-                              color: Colors.grey.shade500,
+                              color: Colors.grey.shade600,
                             ),
                           )
                         ],
@@ -155,6 +207,11 @@ class _VideoCommentsState extends State<VideoComments> {
                               cursorColor: Theme.of(context).primaryColor,
                               decoration: InputDecoration(
                                 hintText: "Add comment...",
+                                hintStyle: TextStyle(
+                                  color: Colors.grey.shade600,
+                                  fontWeight: FontWeight.w300,
+                                  fontSize: Sizes.size16 + Sizes.size1,
+                                ),
                                 border: OutlineInputBorder(
                                   borderRadius: BorderRadius.circular(
                                     Sizes.size10,
@@ -162,42 +219,43 @@ class _VideoCommentsState extends State<VideoComments> {
                                   borderSide: BorderSide.none,
                                 ),
                                 filled: true,
-                                fillColor: Colors.grey.shade200,
+                                fillColor: Colors.grey.shade100,
                                 contentPadding: const EdgeInsets.symmetric(
                                   horizontal: Sizes.size12,
                                 ),
-                                suffixIcon: Padding(
-                                  padding: const EdgeInsets.only(
-                                      right: Sizes.size14),
-                                  child: Row(
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: [
-                                      FaIcon(
-                                        FontAwesomeIcons.at,
-                                        color: Colors.grey.shade900,
-                                      ),
-                                      Gaps.h14,
-                                      FaIcon(
-                                        FontAwesomeIcons.gift,
-                                        color: Colors.grey.shade900,
-                                      ),
-                                      Gaps.h14,
-                                      FaIcon(
-                                        FontAwesomeIcons.faceSmile,
-                                        color: Colors.grey.shade900,
-                                      ),
-                                      Gaps.h14,
-                                      if (_isWriting)
-                                        GestureDetector(
-                                          onTap: _stopWriting,
+                                suffixIcon: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    FaIcon(
+                                      FontAwesomeIcons.at,
+                                      color: Colors.grey.shade900,
+                                    ),
+                                    Gaps.h14,
+                                    FaIcon(
+                                      FontAwesomeIcons.gift,
+                                      color: Colors.grey.shade900,
+                                    ),
+                                    Gaps.h14,
+                                    FaIcon(
+                                      FontAwesomeIcons.faceSmile,
+                                      color: Colors.grey.shade900,
+                                    ),
+                                    Gaps.h14,
+                                    if (_isWriting)
+                                      GestureDetector(
+                                        onTap: _stopWriting,
+                                        child: Padding(
+                                          padding: const EdgeInsets.only(
+                                            right: Sizes.size10,
+                                          ),
                                           child: FaIcon(
                                             FontAwesomeIcons.circleArrowUp,
                                             color:
                                                 Theme.of(context).primaryColor,
                                           ),
                                         ),
-                                    ],
-                                  ),
+                                      ),
+                                  ],
                                 ),
                               ),
                             ),
