efd32b316a58511c9b54b9e904f26e6fccc4e91a
woong <koohw0109@gmail.com>
2023-06-01 19:04:36 +0900

    #11.1 Direct Messages

---
+++ lib/features/inbox/chats_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../../constants/sizes.dart';
+
+class ChatsScreen extends StatefulWidget {
+  const ChatsScreen({super.key});
+
+  @override
+  State<ChatsScreen> createState() => _ChatsScreenState();
+}
+
+class _ChatsScreenState extends State<ChatsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.5,
+        title: const Text("Direct messages"),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const FaIcon(FontAwesomeIcons.plus),
+          ),
+        ],
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(
+          vertical: Sizes.size10,
+        ),
+        children: [
+          ListTile(
+            leading: const CircleAvatar(
+              radius: 30,
+              foregroundImage: NetworkImage(
+                  "https://images.unsplash.com/photo-1685027172781-d7bdd558cf6b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=686&q=80"),
+              child: Text("woong"),
+            ),
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                const Text(
+                  "Bob  ",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Text(
+                  "2:16 PM",
+                  style: TextStyle(
+                    color: Colors.grey.shade500,
+                    fontSize: Sizes.size12 + Sizes.size1,
+                  ),
+                ),
+              ],
+            ),
+            subtitle: const Text("Good morning friend!"),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/features/inbox/inbox_screen.dart
+++ lib/features/inbox/inbox_screen.dart
@@ -2,13 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/inbox/activity_screen.dart';
+import 'package:tiktok_clone/features/inbox/chats_screen.dart';
 
-class InboxScreen extends StatelessWidget {
+class InboxScreen extends StatefulWidget {
   const InboxScreen({super.key});
 
-  void _onDmPressed() {}
+  @override
+  State<InboxScreen> createState() => _InboxScreenState();
+}
 
-  void _onActivityTab(BuildContext context) {
+class _InboxScreenState extends State<InboxScreen> {
+  void _onDmPressed() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const ChatsScreen(),
+      ),
+    );
+  }
+
+  void _onActivityTab() {
     Navigator.of(context).push(
       MaterialPageRoute(
         builder: (context) => const ActivityScreen(),
@@ -39,7 +51,7 @@ class InboxScreen extends StatelessWidget {
       body: ListView(
         children: [
           ListTile(
-            onTap: () => _onActivityTab(context),
+            onTap: _onActivityTab,
             title: const Padding(
               padding: EdgeInsets.symmetric(
                 vertical: Sizes.size20,
