15aa48f1335bdc81136992870ebb73bddc5e57e2
woong <koohw0109@gmail.com>
2023-06-04 04:00:52 +0900

    #12.0 Introduction

---
+++ lib/features/users/user_profile_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class UserProfileScreen extends StatefulWidget {
+  const UserProfileScreen({super.key});
+
+  @override
+  State<UserProfileScreen> createState() => _UserProfileScreenState();
+}
+
+class _UserProfileScreenState extends State<UserProfileScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const CustomScrollView();
+  }
+}
