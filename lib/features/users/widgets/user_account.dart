446500efa17cce2e9ee06d43cb6d961e30b9d918
woong <koohw0109@gmail.com>
2023-06-04 13:34:33 +0900

    userAccount Info code challenge

---
+++ lib/features/users/widgets/user_account.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import '../../../constants/gaps.dart';
+import '../../../constants/sizes.dart';
+
+class UserAccount extends StatelessWidget {
+  final String number;
+  final String infoWord;
+
+  const UserAccount({
+    super.key,
+    required this.number,
+    required this.infoWord,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          number,
+          style: const TextStyle(
+            fontWeight: FontWeight.w700,
+            fontSize: Sizes.size16 + Sizes.size1,
+          ),
+        ),
+        Gaps.v4,
+        Text(
+          infoWord,
+          style: TextStyle(
+            color: Colors.grey.shade600,
+            fontSize: Sizes.size12 + Sizes.size1,
+          ),
+        ),
+      ],
+    );
+  }
+}
