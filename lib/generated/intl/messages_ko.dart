10831576e897cd32a8819591c98ea17243225c8d
woong <koohw0109@gmail.com>
2023-06-13 19:31:20 +0900

    #17.0 Introduction

--- lib/generated/intl/messages_ko.dart
+++ lib/generated/intl/messages_ko.dart
@@ -34,7 +34,7 @@ class MessageLookup extends MessageLookupByLibrary {
   static String m6(videoCount) =>
       "Create a profile, follow other accounts, make your own videos, and more.";
 
-  static String m7(nameOfTheApp, when) => "${nameOfTheApp}에 가입하세요 ${when}";
+  static String m7(nameOfTheApp) => "${nameOfTheApp}에 가입하세요";
 
   final messages = _notInlinedMessages(_notInlinedMessages);
   static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
