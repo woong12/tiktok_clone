27a1423fdb473372145447cd6fb25064522db4e4
woong <koohw0109@gmail.com>
2023-07-19 19:46:38 +0900

    #19.3 Selfie Mode

--- lib/features/users/widgets/video_recording_screen.dart
+++ lib/features/users/widgets/video_recording_screen.dart
@@ -15,7 +15,9 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
   bool _hasPermission = false;
   bool _permissionDenied = false;
 
-  late final CameraController _cameraController;
+  bool _isSelfieMode = false;
+
+  late CameraController _cameraController;
 
   Future<void> initCamera() async {
     final cameras = await availableCameras();
@@ -25,7 +27,7 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
     }
 
     _cameraController = CameraController(
-      cameras[0],
+      cameras[_isSelfieMode ? 1 : 0],
       ResolutionPreset.ultraHigh,
     );
 
@@ -57,6 +59,12 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
     initPermissions();
   }
 
+  Future<void> _toggleSelfieMode() async {
+    _isSelfieMode = !_isSelfieMode;
+    await initCamera();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -73,6 +81,17 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
                     alignment: Alignment.center,
                     children: [
                       CameraPreview(_cameraController),
+                      Positioned(
+                        top: Sizes.size20,
+                        left: Sizes.size20,
+                        child: IconButton(
+                          color: Colors.white,
+                          onPressed: _toggleSelfieMode,
+                          icon: const Icon(
+                            Icons.cameraswitch,
+                          ),
+                        ),
+                      ),
                     ],
                   ),
       ),
