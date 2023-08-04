a10c910ee6f2143d2db2c05720b9656809b23d28
woong <koohw0109@gmail.com>
2023-08-04 12:14:19 +0900

    #29.5 Targeted Notification

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -49,6 +49,26 @@ export const onLikedCreated = functions.firestore
       .update({
         likes: admin.firestore.FieldValue.increment(1),
       });
+    const video = (await db.collection("videos").doc(videoId).get()).data();
+    if (video) {
+      const creatorUid = video.creatorUid;
+      const user = await (
+        await db.collection("users").doc(creatorUid).get()
+      ).data();
+      if (user) {
+        const token = user.token;
+        await admin.messaging().send({
+          token: token,
+          data: {
+            screen: "123",
+          },
+          notification: {
+            title: "someone liked your video.",
+            body: "Likes + 1 ! Congrats! 💖",
+          },
+        });
+      }
+    }
   });
 
 export const onLikedRemoved = functions.firestore
